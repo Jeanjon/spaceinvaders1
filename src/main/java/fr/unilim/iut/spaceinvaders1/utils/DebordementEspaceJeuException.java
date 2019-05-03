9fa66639fa4a89779dbc168b3f3ac4b5f077dd9c
Valentin Jeanjon <valentin.jeanjon@etu.unilim.fr>
2019-05-03 08:43:32 +0200

    Impossibilite de positionner un vaisseau en dehors du plateau

---
+++ src/main/java/fr/unilim/iut/spaceinvaders1/utils/DebordementEspaceJeuException.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvaders1.utils;
+
+@SuppressWarnings("serial")
+public class DebordementEspaceJeuException extends RuntimeException {
+
+    public DebordementEspaceJeuException(String message) {
+	    super(message);
+    }
+}
--- src/test/java/fr/unilim/iut/spaceinvaders1/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders1/SpaceInvadersTest.java
@@ -6,6 +6,7 @@ package fr.unilim.iut.spaceinvaders1;
     
     import org.junit.Test;
 
+import fr.unilim.iut.spaceinvaders1.utils.DebordementEspaceJeuException;
 import fr.unilim.iut.spaceinvaders1.utils.HorsEspaceJeuException;
 
     public class SpaceInvadersTest {
@@ -175,5 +176,23 @@ import fr.unilim.iut.spaceinvaders1.utils.HorsEspaceJeuException;
 			"...............\n" + 
 			".......VVV.....\n" + 
 			".......VVV.....\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+		}
+	    
+	    @Test
+		public void test_UnNouveauVaisseauPositionneDansEspaceJeuMaisAvecDimensionTropGrande_DoitLeverUneExceptionDeDebordement() {
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(9,2,7,9);
+				fail("Dépassement du vaisseau à droite en raison de sa longueur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+			} catch (final DebordementEspaceJeuException e) {
+			}
+			
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(3,4,7,1);
+				fail("Dépassement du vaisseau vers le haut en raison de sa hauteur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+			} catch (final DebordementEspaceJeuException e) {
+			}
+				
 		}
 }
